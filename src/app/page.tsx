b9db236888c33c4c7bfbf132cbf4e20368c58145
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2026-04-09 15:06:39 -0700

    optimizando la pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
+import dynamic from "next/dynamic";
 import Hero from "@/src/components/Hero";
-import Services from "@/src/components/Services"; 
-import Experience from "@/src/components/Experience";
-import Projects from "@/src/components/Projects";
-import Photography from "@/src/components/Photography";
-import TechStack from "@/src/components/TechStack";
-import Footer from "@/src/components/Footer";
 import ThemeToggle from "@/src/components/ThemeToggle";
 import LanguageToggle from "@/src/components/LanguageToggle";
+
+const Services = dynamic(() => import("@/src/components/Services"), { ssr: true });
+const Experience = dynamic(() => import("@/src/components/Experience"), { ssr: true });
+const Projects = dynamic(() => import("@/src/components/Projects"), { ssr: true });
+const TechStack = dynamic(() => import("@/src/components/TechStack"), { ssr: true });
+const Footer = dynamic(() => import("@/src/components/Footer"), { ssr: true });
+
 export default function Home() {
   return (
     <main className="relative w-full overflow-clip">
+
       <ThemeToggle />
       <LanguageToggle />
       <Hero />
+      
+
       <Services />
       <Experience />
       <Projects />
