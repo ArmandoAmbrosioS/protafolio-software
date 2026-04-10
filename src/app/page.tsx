a030a4534ac5069f06d882b325310e238887c8b9
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2026-04-10 05:27:22 -0700

    optimizando la pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Hero from "@/src/components/Hero";
 import ThemeToggle from "@/src/components/ThemeToggle";
 import LanguageToggle from "@/src/components/LanguageToggle";
 
-const Services = dynamic(() => import("@/src/components/Services"), { ssr: true });
-const Projects = dynamic(() => import("@/src/components/Projects"), { ssr: false });
-const TechStack = dynamic(() => import("@/src/components/TechStack"), { ssr: false });
-const Experience = dynamic(() => import("@/src/components/Experience"), { ssr: false });
-const Footer = dynamic(() => import("@/src/components/Footer"), { ssr: true });
+const Services = dynamic(() => import("@/src/components/Services"));
+const Projects = dynamic(() => import("@/src/components/Projects"));
+const TechStack = dynamic(() => import("@/src/components/TechStack"));
+const Experience = dynamic(() => import("@/src/components/Experience"));
+const Footer = dynamic(() => import("@/src/components/Footer"));
 
 export default function Home() {
   return (
